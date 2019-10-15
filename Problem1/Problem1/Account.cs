ae1be034d983d9cc093eac5d87f8097ea29215ba
Nguyễn Thanh Duy Lân <lannguyenthanhduy@gmail.com>
2019-10-15 17:57:14 +0700

    Exercise6

---
+++ Problem1/Problem1/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+        public class Account
+        {
+            private int money;
+            private int wallet;
+            public Account(int amount)
+            {
+                amount = money;
+                money = wallet;
+            }
+            public void Deposit(int amount)
+            {
+                amount += money;
+            }
+            public void withDraw(int amount)
+            {
+                amount -= money;
+            }
+            public int Balance(int amount)
+            {
+                return money;
+            }
+        }
+    }
---
+++ Problem1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodAccount()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(0, acc.Balance(200000));
+        } 
+    }
+}
